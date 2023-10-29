e8fa9c5293fa87e91e0e80b507a1fba4c735a86d
Akinolaaa <akin.akinleye619@gmail.com>
2023-10-29 22:21:26 +0100

    Update course_schedule.py

--- 4. Graphs/course_schedule.py
+++ 4. Graphs/course_schedule.py
@@ -1,18 +1,35 @@
 # https://leetcode.com/problems/course-schedule/
 
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        dictt = {}
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        hashMap = { i:[] for i in range(numCourses)} # initalize each course with an empty list
 
-        for pair in prerequisites:
-            i, j = pair
-            dictt[i] = j
-            if(j in dictt):
+        for crs, pre in prerequisites:
+            hashMap[crs].append(pre)
+        
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if hashMap == []:
+                return True
+            
+            visited.add(crs)
+            for pre in hashMap[crs]:
+                if not dfs(pre):
+                    return False
+                visited.remove(crs)
+                hashMap[crs] = []
+                return True
+        
+        for course in range(numCourses):
+            if not dfs(course):
                 return False
         return True
+            
 
-
-
+            
     # first solution. Only passed 42/52 test cases
     #  failed:
     # 5,  [[1,4],[2,4],[3,1],[3,2]]
@@ -26,3 +43,4 @@ class Solution:
                 return False
         return True
         
+print(Solution().canFinish(5, [[1,4],[2,4],[3,1],[3,2]]))
