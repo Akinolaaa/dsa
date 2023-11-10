9ea6c3d1e51706b45bd6391f9859bf7e094bc9e5
Akinolaaa <akin.akinleye619@gmail.com>
2023-11-10 10:28:19 +0100

    added neetcode solution

--- 5. Greedy/jump_game_ii.py
+++ 5. Greedy/jump_game_ii.py
@@ -1,20 +1,32 @@
 # leetcode 45- jump game 2
 
 import collections
-
-
+# Solution similar to algorithm for minimizing a DFA
 class Solution:
-    # Solution similar to algorithm for minimizing a DFA
+    # neetcode solution
+    def jump(self, nums: list[int]) -> int:
+        res = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
+
+
+
     def minimumJumps(self, nums: list[int]) -> int:
         q = collections.deque()
         q.append(0)
         visited = set()
         jumps = 0
         while q:
-            print("----jump")
             for i in range(len(q)):
                 j = q.popleft()
-                print(j)
                 if j == len(nums) - 1:
                     return jumps
                 for a in range(j + 1, j + nums[j] + 1):
@@ -35,3 +47,4 @@ numbers2 = [3, 4, 3, 2, 5, 4, 3]
 numbers3 = [9, 8, 2, 2, 0, 2, 2, 0, 4, 1, 5, 7, 9, 6, 6, 0, 6, 5, 0, 5]
 
 print(Solution().minimumJumps(numbers3))
+print(Solution().jump(numbers3))
