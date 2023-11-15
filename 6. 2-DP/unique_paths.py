d05d42f26e4f4cb8ddda20f0d1e649a1a0226eb3
Akinolaaa <akin.akinleye619@gmail.com>
2023-11-15 15:44:23 +0100

    62- solved successfully :)

---
+++ 6. 2-DP/unique_paths.py
@@ -0,0 +1,21 @@
+# leetcode 62-https://leetcode.com/problems/unique-paths/description/
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        dp = [
+            [0 for _ in range(n + 1)] for i in range(m + 1)
+        ]  # extra layer for index-out-of-bounds problem
+        dp[m - 1][n - 2] = 1
+        dp[m - 2][n - 1] = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if dp[i][j] != 0:
+                    continue
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
+        return dp[0][0]
+
+
+sol = Solution().uniquePaths(1, 1)
+print(sol)
