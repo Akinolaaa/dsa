eddb9380af8bb8813ede384c0748add2b498c366
Akinolaaa <akin.akinleye619@gmail.com>
2023-11-16 10:49:22 +0100

    slightly more optimal solution

--- 6. 2-DP/unique_paths.py
+++ 6. 2-DP/unique_paths.py
@@ -16,6 +16,18 @@ class Solution:
                 dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
         return dp[0][0]
 
+    # neetcode algorithm. Essentially the same but it saves time
+    def uniquePaths1(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for _ in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+                row = newRow
+
+            return row[0]
+
 
 sol = Solution().uniquePaths(1, 1)
 print(sol)
