885673c5e8471835589d0feb1d7275d9955d972d
Akinolaaa <akin.akinleye619@gmail.com>
2023-11-27 16:38:01 +0100

    worked on this advanced graph problem, solved 300

---
+++ 3. 1-DP/longest_increasing_subsequence.py
@@ -0,0 +1,19 @@
+# 300
+
+
+class Solution:
+    def longestIncreasingSubsequence(self, nums: list[int]) -> int:
+        dp = [1] * len(nums)
+        longest = 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+                    longest = max(longest, dp[i])
+
+        return longest
+
+
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+print(Solution().longestIncreasingSubsequence(nums))
---
+++ 8. Advanced Graphs/reconstruct_itinery.py
@@ -0,0 +1,36 @@
+import collections
+
+
+class Solution:
+    def findItinerary(self, tickets: list[list[str]]) -> list[str]:
+        # construct adjacency list
+        graph = collections.defaultdict(lambda: [])
+
+        tickets.sort()  # sorts by first item then second if first is equal instead of sorting each list
+        for f, t in tickets:
+            graph[f].append(t)
+
+        # for port in graph.keys():
+        #     graph[port].sort()
+
+        res = ["JFK"]
+
+        def dfs(port):
+            if len(res) == len(tickets) + 1:  # this means we have traversed every node
+                return True
+            if (
+                port not in graph
+            ):  # this means it  has no outgoing edges and we get stuck
+                return False
+            temp = list(graph[port])  # getting storing this so we can mutate it
+            for i, dest in enumerate(temp):
+                graph[port].pop(i)  # assume this edge is a viable solution
+                res.append(dest)  # add to res
+                if dfs(dest):  # if viable
+                    return True
+                # else put it back in the list and remove it(like backtracking)
+                graph[port].insert(i, dest)
+                res.pop()
+
+        dfs("JFK")
+        return res
