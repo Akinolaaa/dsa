fb01a4c06627c246e41685cb95b52cd52d085198
Akinolaaa <akin.akinleye619@gmail.com>
2023-12-14 15:07:42 +0100

    success- reverse k-group

---
+++ 7. LinkedLists/reverse_k_group.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+        while True:
+            kth = self.findkth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            # reverse
+            prev, cur = kth.next, groupPrev.next
+            tail = cur
+            while cur != groupNext:
+                temp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp
+            groupPrev.next = kth
+            tail.next = groupNext
+            groupPrev = tail
+        return dummy.next
+
+    def findkth(self, start, k):
+        cur = start
+        while cur and k > 0:
+            cur = cur.next
+            k -= 1
+        return cur
